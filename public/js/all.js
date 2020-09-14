5c85b47fa9e5193044bed5f5f49c159e46b8ae23
james HE <james840525@gmail.com>
2020-09-14 16:55:12 +0800

    demo_v1

--- public/js/all.js
+++ public/js/all.js
@@ -3,7 +3,13 @@
 $(document).ready(function () {
   $('#submit').click(function () {
     var plates = $('#plates').val();
-    console.log(plates);
+    var mesa = $('#mesa').val();
+    var Barrel = $('#Barrel').val();
+    var barrelMaterial = $('#barrelMaterial').val();
+    var customer = $('#customer').val();
+    var str = '';
+    str += "\n        <tr>\n            <th scope=\"row\">".concat(customer, "</th>\n            <td>").concat(plates, "</td>\n            <td>").concat(mesa, "</td>\n            <td>").concat(Barrel, "</td>\n            <td>").concat(barrelMaterial, "</td>\n        </tr>\n        ");
+    $('#add_meta').append(str);
   });
 });
 //# sourceMappingURL=all.js.map
--- source/js/all.js
+++ source/js/all.js
@@ -1,7 +1,21 @@
 $(document).ready(function () {
     $('#submit').click(function () {
         const plates = $('#plates').val();
-        console.log(plates);
+        const mesa = $('#mesa').val();
+        const Barrel = $('#Barrel').val();
+        const barrelMaterial = $('#barrelMaterial').val();
+        const customer = $('#customer').val();
+        let str = '';
 
+        str += `
+        <tr>
+            <th scope="row">${customer}</th>
+            <td>${plates}</td>
+            <td>${mesa}</td>
+            <td>${Barrel}</td>
+            <td>${barrelMaterial}</td>
+        </tr>
+        `
+        $('#add_meta').append(str);
     });
 });
